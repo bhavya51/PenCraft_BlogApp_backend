781bfdc033d68bcdd137356cf44f69f4fd730e9d
Bhavya Lalchandani <bhavyal321@gmail.com>
2023-11-10 15:03:44 +0530

    origin update

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "https://pencraft-the-blogging-app.vercel.app/", credentials: true }));
+app.use(cors({ 
+  origin: [process.env.FRONT_END_URL],
+  methods:["GET","POST","PUT","DELETE"],
+  credentials: true 
+}));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
